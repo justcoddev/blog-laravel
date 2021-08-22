b3f14f9e90a0757ed3fb38fdb42d491d17649507
Teddy <edisonpaulcrz@gmail.com>
2021-08-21 22:10:08 -0500

    dejamos original

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,6 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\LenguajeController;
-
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -14,15 +13,10 @@ use App\Http\Controllers\LenguajeController;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', HomeController::class);
-
-
-
 //OTRA MANERA DE DEFINIR RUTAS  luego de haberlas creado es la siguiente
 //ademàs tambien le asigna mètodos como get, post, put y delete se puede saber 
 //llendo a consola y colocando
 //php artisan r:l
 //listar las rutas
-Route::resource('modelos', LenguajeController::class)->parameters(['modelos'=>'lenguaje'])->names('lenguajes');
-//de esta manera puedo cambiar la ruta principal y no tener problemas con al secundario
+Route::resource('lenguajes', LenguajeController::class);
